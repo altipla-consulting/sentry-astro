7a073da68078524ed5cc975234371314f8ceb376
Ernesto Alejo <ernestokarim@gmail.com>
2025-01-11 11:30:25 +0100

    Log tRPC errors when silenced.

--- src/trpc.ts
+++ src/trpc.ts
@@ -2,19 +2,19 @@ import { setContext } from '@sentry/node'
 import { getHTTPStatusCodeFromError } from '@trpc/server/http'
 import { TRPCError } from '@trpc/server'
 import { captureError } from './capture.js'
+import { logger } from '@altipla/logging'
 
 export function sentryTRPC({ error, input, path }: any) {
   setContext('trpc', { input })
 
-  if (process.env.SENTRY_DSN) {
-    // Silence client code validation errors.
-    if (error instanceof TRPCError) {
-      let code = getHTTPStatusCodeFromError(error)
-      if (code === 404 || code === 400) {
-        throw error
-      }
+  // Silence client code validation errors.
+  if (error instanceof TRPCError) {
+    let code = getHTTPStatusCodeFromError(error)
+    if (code === 404 || code === 400) {
+      logger.error(error)
+      throw error
     }
-
-    captureError(error, { path })
   }
+
+  captureError(error, { path })
 }
